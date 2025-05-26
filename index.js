c294f60971974c585cd444b61df08b5e9bb72d96
unknown <shreyasdk28@gmail.com>
2025-05-26 16:10:01 +0530

    Updated index.js and added start script

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const soap = require("soap");
 const bodyParser = require("body-parser");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const wsdlUrl = "http://www.dneonline.com/calculator.asmx?WSDL";
 
 app.use(bodyParser.urlencoded({ extended: true }));
